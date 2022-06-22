47dbdc37e6c0e7382a95f4642dd405104f38763f
meet10102020 <108017233+meet10102020@users.noreply.github.com>
2022-06-22 21:52:02 +0530

    Add files via upload

---
+++ 1.a.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+float f,c;
+
+// cout<<"enter the temprature in fahrenite  "<<endl;
+
+// cin>>f;
+
+// c=(f-32)*5/9;
+
+// cout<<"the temprature in celsuis"<<c<<endl;
+
+cout<<"enter the temprature in celsuis "<<endl;
+
+cin>>c;
+
+f=(c*9)/5+32;
+
+cout<<"the temprature in fahrenite "<<f<<endl;
+
+}
